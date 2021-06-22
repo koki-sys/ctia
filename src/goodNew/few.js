9105c256813ed16ba4d985d2aba86e6b9b6f79d3
koki-sys <soraisu7@gmail.com>
2021-06-22 23:38:42 +0900

    QRコード部分を修正しました。

--- src/goodNew/few.js
+++ src/goodNew/few.js
@@ -1,37 +1,27 @@
-exports.few = (socket, IOserver) => {
-    const { groupInit } = require('./groupInit');
-
-    // 少人数用の部屋を作製
-    let fewGoodNewRoomArray = Array(50);
-    groupInit(fewGoodNewRoomArray);
-
+exports.few = (socket, IOserver, room) => {
     /**
     * 少人数用の部屋参加関数(ニックネーム入力画面から送信)
     *
     * @emits {String} data.entryRoomName 部屋名
-    * @emits {Number} data.roomCount 部屋数
     * @emits {Number} data.limitPerRoom 部屋あたりの制限人数
     * @emits {Number} data.countInRoom 部屋内の人数
     */
 
     socket.on("few_group", (data) => {
-        console.log(socket);
-        console.log(IOserver);
-        for (let roomNumber = 0; roomNumber < fewGoodNewRoomArray.length; roomNumber++) {
-            console.log(fewGoodNewRoomArray[roomNumber]);
-            if (fewGoodNewRoomArray[roomNumber] < 13) {
-                fewGoodNewRoomArray[roomNumber] += 1;
-                console.log(fewGoodNewRoomArray[roomNumber]);
+        for (let roomNumber = 0; roomNumber < room.length; roomNumber++) {
+            if (room[roomNumber] < 13) {
+                room[roomNumber] += 1;
+                console.log("現在部屋内の人数:" + room[roomNumber]);
                 const entryRoomName = (data.roomName == null) ? "部屋" + (roomNumber + 1) : data.roomName;
                 socket.join(entryRoomName);
-                const countInRoom = fewGoodNewRoomArray[roomNumber];
+                const countInRoom = room[roomNumber];
 
-                    IOserver.to(entryRoomName).emit("generate", {
-                        entryRoomName: entryRoomName,
-                        limitPerRoom: 13,
-                        countInRoom: countInRoom,
-                    });
-                    console.log("emitted.");
+                IOserver.to(entryRoomName).emit("generate", {
+                    entryRoomName: entryRoomName,
+                    limitPerRoom: 13,
+                    countInRoom: countInRoom,
+                });
+                console.log("emitted.");
                 break;
             }
         }
