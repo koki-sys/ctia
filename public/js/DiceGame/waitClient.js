9b135103d4945ca4c3dcac8742284c5f0bc54f90
koki-sys <soraisu7@gmail.com>
2021-07-11 15:39:07 +0900

    QRの修正、その他細かな修正を行いました。

--- public/js/DiceGame/waitClient.js
+++ public/js/DiceGame/waitClient.js
@@ -1,5 +1,9 @@
 import { dgClientIO, completeGroupingUrl } from '../../link.js';
 
+// セッション取得
+const nickNameFromSession = sessionStorage.getItem('nickName');
+const roomCountFromSession = sessionStorage.getItem('roomCount');
+const limitPerRoomFromSession = sessionStorage.getItem('limitPerRoom');
 
 /**
  * ニックネームなどをサーバーに送信する関数
@@ -10,34 +14,33 @@ import { dgClientIO, completeGroupingUrl } from '../../link.js';
 */
 window.addEventListener('DOMContentLoaded', () => {
     console.log("loaded");
-    console.log(sessionStorage.getItem('nickNameFromClient'));
-    console.log(sessionStorage.getItem('clientRoomCount'));
-    console.log(sessionStorage.getItem('clientRoomLimit'));
+    console.log("ニックネームは、" + nickNameFromSession);
+    console.log("部屋数は、" + roomCountFromSession);
+    console.log("制限人数は、" + limitPerRoomFromSession);
     dgClientIO.emit("set_nickname", {
-        nickName: sessionStorage.getItem('nickNameFromClient'),
-        roomCount: sessionStorage.getItem('clientRoomCount'),
-        limitPerRoom: sessionStorage.getItem('clientRoomLimit')
+        nickName: nickNameFromSession,
+        roomCount: roomCountFromSession,
+        limitPerRoom: limitPerRoomFromSession
     })
 });
 
 dgClientIO.on("waiting", (data) => {
     console.log("waiting");
     $('ul').append('<li class="list-group-item">' + data.nickName + 'さん</li>');
-    const nickNameFromClient = sessionStorage.getItem('nickNameFromClient');
     const nickNameFromServer = data.nickName;
-    const countInRoom = parseInt(data.countInRoom);
-    const limitPerRoom = parseInt(data.limitPerRoom);
-    console.log(countInRoom)
-    console.log(limitPerRoom)
+    const countInRoomFromServer = parseInt(data.countInRoom);
+    const limitPerRoomFromServer = parseInt(data.limitPerRoom);
+    console.log("受信した部屋数：" + countInRoomFromServer);
+    console.log("受信した制限人数:" + limitPerRoomFromServer);
     // 自分が入力したニックネームと一致しているのを確認
-    if (nickNameFromServer == nickNameFromClient) {
+    if (nickNameFromServer == nickNameFromSession) {
         sessionStorage.setItem('nickName', data.nickName);
     }
 
     // 人数が揃ったら自動遷移
-    if (countInRoom == limitPerRoom) {
+    if (countInRoomFromServer == limitPerRoomFromServer) {
         // グループ割り振り完了画面に送信
-        sessionStorage.setItem('countInRoom', countInRoom);
+        sessionStorage.setItem('countInRoom', countInRoomFromServer);
         sessionStorage.setItem('entryRoomName', data.entryRoomName);
         location.href = completeGroupingUrl;
     }
