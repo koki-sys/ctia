651a15275ff8e9c8123e8b84ff0026fc860922a9
koki-sys <soraisu7@gmail.com>
2021-07-22 17:35:13 +0900

    データベースを追加しました。

---
+++ src/dicegame/getUserCount.js
@@ -0,0 +1,12 @@
+exports.getUserCount = async (random) => {
+
+    const { mycon } = require("../connectDB");
+
+    // ルーム内のユーザ数をカウント
+    const getUserCountParam = random;
+    const getUserCountSQL = 'SELECT COUNT(id) AS cnt FROM user WHERE room_id = ?';
+    const [userInRoom] = await mycon.query(getUserCountSQL, getUserCountParam);
+    const countInRoom = userInRoom[0].cnt;
+
+    return countInRoom;
+}
