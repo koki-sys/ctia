18256a48d34ceea4208bcb035af2d09d23e3a894
koki-sys <soraisu7@gmail.com>
2021-08-06 19:03:25 +0900

    回答、画面遷移を修正しました。

---
+++ public/js/nameGame/component/link/toWait.js
@@ -0,0 +1,6 @@
+// ゲーム画面に遷移する
+const toWait = async () => {
+    document.location.href = "./namegame.html";
+}
+
+export { toWait }
--- public/js/nameGame/game/correctAnswerer.js
+++ public/js/nameGame/game/correctAnswerer.js
@@ -1,10 +1,29 @@
+import { toWait } from '../component/link/toWait.js';
+import { ngClientIO } from '../../../link.js';
+
 // html要素取得
+const answerBtnElement = document.getElementById('answer-btn');
 const answererElment = document.getElementById('correct-answerer');
 
 // session取得
 const correctAnswerer = sessionStorage.getItem('correctAnswerer');
+const roomCount = sessionStorage.getItem('roomCount');
+const countInRoom = sessionStorage.getItem('countInRoom');
 
 // ロード時に画面に正解者を表示
 window.onload = () => {
     answererElment.textContent = correctAnswerer + "さん";
+    ngClientIO.emit('waitInit', {});
 }
+
+answerBtnElement.onclick = () => {
+    ngClientIO.emit('waitConfirm', {
+        roomCount: roomCount,
+        countInRoom: countInRoom
+    })
+}
+
+// 全員が確認したことを受信したときに画面遷移する処理
+ngClientIO.on('toNameGame', () => {
+    toWait();
+})
