5e5f8491e40828bf0844ce192dde2c5ba04ac0a3
koki-sys <soraisu7@gmail.com>
2021-08-10 19:05:23 +0900

    グッドアンドニューの修正をしました。

--- src/controller/goodNew/gnController.js
+++ src/controller/goodNew/gnController.js
@@ -1,9 +1,34 @@
 const { order } = require('../../model/order');
-const { requestOrderPattern } = require('../../component/order/requestOrderPattern');
+const { user } = require('../../model/user');
+const { randomPattern } = require("../../component/order/randomPattern");
 
 exports.gnController = (socket, IOserver) => {
 
-    requestOrderPattern(socket, IOserver);
+    socket.on("requestOrderPattern", async (data) => {
+        if (data.flg != "answered") {
+            const roomId = data.roomId;
+            const nickname = data.nickname;
+
+            // 順番作製のために
+            const orderPattern = randomPattern();
+
+            const PersonInfo = await user.find(nickname);
+            const userId = PersonInfo.id;
+
+            const orderData = {
+                roomId: roomId,
+                userId: userId,
+                random: orderPattern
+            }
+
+            await order.add(orderData);
+
+            // 順番を受け取りに来たユーザに順番を送る。
+            IOserver.to(socket.id).emit("sendOrderPattern", {
+                orderPattern: orderPattern,
+            });
+        }
+    });
 
     socket.on("order", async (data) => {
         if (data.flg == "answered") {
