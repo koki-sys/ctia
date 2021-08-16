cac6eeba23f4586205ee76aa70c367b119b8f645
koki-sys <soraisu7@gmail.com>
2021-08-16 23:52:43 +0900

    順番機能のフロント部分を修正

--- public/js/illustGame/taskComplete.js
+++ public/js/illustGame/taskComplete.js
@@ -1,16 +1,31 @@
 import { igClientIO } from "../../link.js"
-import { change } from "../component/game/change.js"
-import { sendOrder } from "../component/game/sendOrder.js";
+import { everyTimechange } from "../component/game/everyTimechange.js";
 import { setOrder } from "../component/game/setOrder.js";
 
 window.onload = () => {
     igClientIO.emit("realtime-draw", {
         act: "towait",
     })
-    
-    sendOrder();
 
-    change(igClientIO);
+    everyTimechange(igClientIO);
 }
 
-setOrder();
+setOrder(igClientIO);
+
+// 順番が自分に回ってきたら遷移する。
+igClientIO.on('changeOrder', (data) => {
+    const pattern = String(data.changePattern);
+    const orderPattern = sessionStorage.getItem('orderPattern');
+    console.log("'" + pattern + "'");
+    console.log("'" + orderPattern + "'");
+
+    // 順番が来たら、発表画面に切り替える。
+    console.log("junnbannkae");
+    if (orderPattern === pattern) {
+        toAnnounce();
+    }
+})
+
+igClientIO.on('gameEnd', () => {
+    toGameEnd();
+})
