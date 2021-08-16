aae7ee57cb8cc5659d610780a3c8966839b43986
koki-sys <soraisu7@gmail.com>
2021-08-16 23:10:26 +0900

    順番機能のフロント部分を作成いたしました。

--- public/js/illustGame/taskComplete.js
+++ public/js/illustGame/taskComplete.js
@@ -1,7 +1,16 @@
 import { igClientIO } from "../../link.js"
+import { change } from "../component/game/change.js"
+import { sendOrder } from "../component/game/sendOrder.js";
+import { setOrder } from "../component/game/setOrder.js";
 
 window.onload = () => {
     igClientIO.emit("realtime-draw", {
         act: "towait",
     })
+    
+    sendOrder();
+
+    change(igClientIO);
 }
+
+setOrder();
