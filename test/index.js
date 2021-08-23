56d15e07a811bef06160ec8f5d177e357c5172c3
koki-sys <soraisu7@gmail.com>
2021-08-23 15:11:21 +0900

    テストファイルのリファクタリング

--- test/index.js
+++ test/index.js
@@ -1,16 +1,3 @@
-const { namegameModelTest } = require("./model/namegame");
-const { orderModelTest } = require("./model/order");
-const { roomModelTest } = require("./model/room");
-const { userModelTest } = require("./model/user");
-const { typingModelTest } = require('./model/typing');
-
-// それぞれのテストファイルをインポートする
-const ModelTest = () => {
-    roomModelTest();
-    userModelTest();
-    orderModelTest();
-    namegameModelTest();
-    typingModelTest();
-}
+const { ModelTest } = require("./model");
 
 ModelTest();
